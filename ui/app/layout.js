de35ea436c10ff0f0918c4ef22faba679f07b054
Nikhil PN <nikhilpn725@gmail.com>
2025-05-31 17:17:46 +0530

    embed added testign

--- ui/app/layout.js
+++ ui/app/layout.js
@@ -11,14 +11,42 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const frameEmbed = {
+  version: "1",
+  imageUrl: "https://www.youbet.fun/youbet.png",
+  button: {
+    title: "Open YouBet",
+    action: {
+      type: "launch_frame",
+      name: "YouBet",
+      url: "https://www.youbet.fun",
+      splashImageUrl: "https://www.youbet.fun/youbet.png",
+      splashBackgroundColor: "#ffffff",
+    },
+  },
+};
+
 export const metadata = {
   title: "YouBet",
-  description: "Bet on anything",
+  description:
+    "Bet on anything - Discover and create bets in a decentralized betting platform",
+  metadataBase: new URL("https://www.youbet.fun"),
+  openGraph: {
+    title: "YouBet",
+    description: "Bet on anything",
+    images: ["https://www.youbet.fun/youbet.png"],
+  },
+  other: {
+    "fc:frame": JSON.stringify(frameEmbed),
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="fc:frame" content={JSON.stringify(frameEmbed)} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
